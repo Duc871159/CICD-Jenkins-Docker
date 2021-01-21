c080a99b0e550d9744b624f846e688c5352f8d32
duc871159 <ducnguyen871159@gmaill.com>
2021-01-21 16:02:55 +0700

    Add Test

--- src/test/java/com/example/cicddemo/CicddemoApplicationTests.java
+++ src/test/java/com/example/cicddemo/CicddemoApplicationTests.java
@@ -1,13 +1,59 @@
 package com.example.cicddemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CicddemoApplicationTests {
+public class CicddemoApplicationTests {
+
+    public final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        try {
+            testCalculatorAdd(1,2,3);
+            testCalculatorMinus(5, 1, 4);
+        } catch (AssertionError e) {
+            fail("Loi: " + e.getMessage());
+        }
+    }
+
+    public void testCalculatorAdd(int x, int y, int sum) {
+        try {
+            assertEquals(x + y, sum);
+            System.out.println("SUCCESS");
+        } catch (AssertionError e) {
+            throw new AssertionError("Error: " + x + " + " + y + "=" + sum);
+        }
+    }
+
+    public void testCalculatorMinus(int x, int y, int minus) {
+        try {
+            assertEquals(x - y, minus);
+            System.out.println("SUCCESS");
+        } catch (AssertionError e) {
+            throw new AssertionError("Error: " + x + " - " + y + "=" + minus);
+        }
+    }
+
+    @After()
+    public void cleanUpStreams() {
+        System.setOut(null);
     }
 
 }
